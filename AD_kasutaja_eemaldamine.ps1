76e80432d93cd885d16c9297d17022881c570b53
Martin Mäerand <martin.maerand@voco.ee>
2022-03-28 22:19:46 +0300

    Kasutajate lisamine ja eemaldamine

---
+++ AD_kasutaja_eemaldamine.ps1
@@ -0,0 +1,42 @@
+﻿$firstname = Read-Host "Sisesta eesnimi"
+$lastname = Read-Host "Sisesta perekonnanimi"
+$username = $firstname + "." + $lastname
+$username = $username.ToLower()
+$username = Translit($username)
+$displayname = $firstname + " " + $lastname
+
+    if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+                Remove-ADUser -identity $username
+              echo "Kasutaja $displayname eemaldatud edukalt."
+       }
+       else
+       {
+       Write-Warning "Kasutajat ei eksisteeri. Proovi uuesti või kontrolli nime"
+        
+        }
+
+    
+
+function Translit {
+    param(
+        [string] $inputString
+    )
+
+        $Translit = @{
+        [char]'ä' = "a"
+        [char]'õ' = "o"
+        [char]'ü' = "u"
+        [char]'ö' = "o"
+        }
+        $outputString=""
+        foreach ($character in $inputCharacter = $inputString.ToCharArray())
+    {   
+        if ($Translit[$character] -cne $Null){
+        $outputString += $Translit[$character]
+        }else {
+            $outputString += $character
+            }
+            }
+            Write-Output $outputString
+            }
---
+++ AD_kasutaja_loomine.ps1
@@ -0,0 +1,53 @@
+﻿$file = "C:\Users\Administrator\Documents\Win_scripts\kasutajad.csv"
+$ErrorActionPreference = 'SilentlyContinue'
+#Impordi faili sisu
+$users = Import-Csv $file -Encoding Default -Delimiter ";"
+#foreach user row in file
+foreach ($user in $users){
+    $username = $user.FirstName + "." + $user.LastName
+    $username = $username.ToLower()
+    $username = Translit($username)
+    $upname = $username + "@sv-kool.local"
+    $displayname = $user.FirstName + " " + $user.LastName
+    if (Get-ADUser -F {SamAccountName -eq $Username})
+       {
+               
+               Write-Warning "Kasutaja $Username juba olemas - kahjuks ei saa lisada."
+       }
+       else
+       {
+        New-ADUser -Name $username `
+        -DisplayName $displayname `
+        -GivenName $user.FirstName `
+        -Title $user.Role `
+        -Surname $user.LastName `
+        -Department $user.Department `
+        -UserPrincipalName $upname `
+        -AccountPassword (ConvertTo-SecureString $user.Password -AsPlainText -force) -Enabled $true
+         echo "Uus kasutaja $Username edukalt lisatud."
+        }
+
+    
+}
+function Translit {
+    param(
+        [string] $inputString
+    )
+
+        $Translit = @{
+        [char]'ä' = "a"
+        [char]'õ' = "o"
+        [char]'ü' = "u"
+        [char]'ö' = "o"
+        }
+        $outputString=""
+        foreach ($character in $inputCharacter = $inputString.ToCharArray())
+    {   
+        if ($Translit[$character] -cne $Null){
+        $outputString += $Translit[$character]
+        }else {
+            $outputString += $character
+            }
+            }
+            Write-Output $outputString
+            }
